bd6f37c8b133e47b18f5345c32cbcadb031443e8
parthtpujara@gmail.com <ParthtPujara@gmail.com>
2023-09-26 11:13:43 +0530

    First Commit

---
+++ pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section1 from '../components/HomePage/Section1'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Section1/>
+    </div>
+  )
+}
+
+export default HomePage
