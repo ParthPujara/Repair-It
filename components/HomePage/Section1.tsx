c2c118a5208d97d246492ce96f3d5945968b88b2
parthtpujara@gmail.com <ParthtPujara@gmail.com>
2023-11-11 11:07:19 +0530

    Home Page Animation added

--- components/HomePage/Section1.tsx
+++ components/HomePage/Section1.tsx
@@ -1,6 +1,7 @@
 import { TypeAnimation } from "react-type-animation";
 import CashOnDelivery from "../../public/Home Animations/CashOnDelivery.mp4";
 import OnPointDelivery from "../../public/Home Animations/OnPointDelivery.mp4";
+import D2DDelivery from "../../public/Home Animations/D2DDelivery.mp4";
 import CountUp from "react-countup";
 
 // import {
@@ -14,14 +15,23 @@ const Section1 = () => {
   return (
     <div>
       <div className="home_section1">
-        <div className="home_section1_left flex justify-center items-center flex-col">
-          We repair
+        <video
+          src={D2DDelivery}
+          autoPlay
+          loop
+          muted
+          ref={(ref) => {ref.playbackRate= 0.8}}
+          playsInline
+          className="home_section1_top_video"
+        />
+        <div className="home_section1_left flex justify-center items-center flex-col text-center">
+          We repair 
           <TypeAnimation
             sequence={["Mobile...", 1000, "Laptop...", 1000]}
             wrapper="span"
             speed={20}
             deletionSpeed={10}
-            // style={{ fontSize: "2em", display: "inline-block" }}
+            style={{ color: "#ff4d72" }}
             repeat={Infinity}
             className=""
           />
@@ -133,12 +143,52 @@ const Section1 = () => {
 
       <div className="home_section3">
         <div className="home_heading">Our Users</div>
-        <div className="home_section3_counters">
-          <div className="mobile+_counter">
-            <div className="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-</svg>
+        <div className="home_section3_counters flex justify-center gap-[50px] flex-wrap">
+          <div className="mobile_counter">
+            <div className="logo flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#fff"
+                className="mobile_svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
+            </div>
+            <CountUp
+              start={0}
+              end={1000}
+              className="counter"
+              delay={1}
+              duration={5}
+              separator=","
+              useEasing={true}
+              enableScrollSpy={true}
+            />
+            <span className="counter">+</span>
+          </div>
+          <div className="mobile_counter">
+            <div className="logo flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#fff"
+                className="mobile_svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
             </div>
             <CountUp
               start={0}
